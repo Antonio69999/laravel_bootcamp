03564c3b6a60c45e2d3658124076d8c281bf128e
Antonio69999 <antoine.chabeauti@gmail.com>
2024-12-18 13:24:56 +0100

    LARAVEL

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use App\Http\Requests\ChirpRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        // return response('Hello World');
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ChirpRequest $request): RedirectResponse
+    {
+
+        $request->user()->chirps()->create($request->validated());
+
+        return redirect()->route('chirps.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ChirpRequest $request, Chirp $chirp): RedirectResponse
+    {
+        Gate::authorize('update', $chirp);
+
+        $chirp->update($request->validated());
+
+        return redirect()->route('chirps.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        Gate::authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect()->route('chirps.index');
+    }
+}
---
+++ app/Http/Requests/ChirpRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+
+class ChirpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // on verifie simplement si l'utilisateur est connecté
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'message' => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'message.required' => 'Le message est obligatoire',
+            'message.string' => 'Le message doit être une chaine de caractère',
+            'message.max' => 'Le message ne doit pas dépasser 255 caractères',
+        ];
+    }
+}
